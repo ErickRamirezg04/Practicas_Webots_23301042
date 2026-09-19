215c95eca90098a689d4ad21a75f15a0b3f78308
ErickRamirezg04 <erick.ramirezg04@gmail.com>
2026-09-18 18:39:39 -0600

    Archivos de Practicas WEBOTS

---
+++ sandbox2/controllers/controlador_pendulo/build/release/controlador_pendulo.d
@@ -0,0 +1,6 @@
+build/release/controlador_pendulo.o: controlador_pendulo.cpp \
+ C:\Users\1105334954\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Users/1105334954/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\1105334954\AppData\Local\Programs\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Users\1105334954\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:\Users\1105334954\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp
